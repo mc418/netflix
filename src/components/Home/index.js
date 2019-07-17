fbda4499f3d98c2db42e862653b117a06ee4edb1
Mia <xxx@happymmall.com>
2019-07-17 15:51:04 -0700

    add logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ import MyList from '../MyList';
 import Recommend from '../Recommend';
 import * as actions from '../../redux/actions';
 import { connect } from 'react-redux';
-
+import logo from './logo.jpg'
 class Home extends Component {
     componentDidMount() {
         this.props.loadList();
@@ -15,7 +15,8 @@ class Home extends Component {
         const {list, recommend} = this.props;
         return (
             <div>
-                <h1>NETFLIX</h1>
+                {/* <h1>NETFLIX</h1> */}
+                <img className="logo" src={logo} alt="NETFLIX" />
                 <MyList />
                 <Recommend rec={recommend} />
                 <List myList={list} />
